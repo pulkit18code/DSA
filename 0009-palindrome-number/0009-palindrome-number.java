2ff14a41c77a637d87d771cae9eafba7ae0b6b3f
Pulkit Sharma <pulkit1865@gmail.com>
2026-06-19 13:19:49 +0530

    Time: 5 ms (79.19%), Space: 45.8 MB (86.72%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int original = x;
+        int rev = 0;
+
+        while (x > 0) {
+            int rem = x % 10;   
+            rev = rev * 10 + rem; 
+            x = x / 10;         
+        }
+
+        return original == rev;
+    }
+}
