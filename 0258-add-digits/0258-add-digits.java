7d06da927d65aa9d679c5355294c5796ce7daaf3
Pulkit Sharma <pulkit1865@gmail.com>
2026-06-20 14:18:11 +0530

    Time: 1 ms (98.13%), Space: 42.6 MB (43.93%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,5 +1,21 @@
 class Solution {
-    public int addDigits(int num) {
-        return num == 0 ? 0 : 1 + (num - 1) % 9;
+    public int addDigits(int num) 
+    {
+        if (num < 10) 
+        {
+            return num;
+        }
+        int sum = sumOfDigits(num);
+
+        return addDigits(sum);
+    }
+
+    int sumOfDigits(int num) 
+    {
+        if (num == 0) 
+        {
+            return 0;
+        }
+        return (num % 10) + sumOfDigits(num / 10);
     }
 }
